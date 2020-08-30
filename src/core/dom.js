7f04a13be6ab90670790c8a5c17442bd84a9990a
alzhab <abd.alz.b@gmail.com>
2020-08-30 13:25:33 +0600

    Finish table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -14,6 +14,19 @@ class Dom {
 		}
 	}
 
+	text(text) {
+		if (typeof text === 'string') {
+			this.$el.textContent = text
+			return this
+		}
+
+		if (this.$el.tagName.toLowerCase() === 'input') {
+			return this.$el.value.trim()
+		} else {
+			return this.$el.textContent.trim()
+		}
+	}
+
 	clear() {
 		this.html('')
 		return this
@@ -25,6 +38,22 @@ class Dom {
 
 	off(eventType, callback) {
 		this.$el.removeEventListener(eventType, callback)
+	}
+
+	id(parse) {
+		if (parse) {
+			const parsed = this.id().split(':')
+			return {
+				row: +parsed[0],
+				col: +parsed[1]
+			}
+		}
+
+		return this.data.id
+	}
+
+	find(selector) {
+		return $(this.$el.querySelector(selector))
 	}
 
 	append(node) {
@@ -63,6 +92,21 @@ class Dom {
 			}
 		}
 	}
+
+	addClass(cl) {
+		this.$el.classList.add(cl)
+		return this
+	}
+
+	removeClass(cl) {
+		this.$el.classList.remove(cl)
+		return this
+	}
+
+	focus() {
+		this.$el.focus();
+		return this
+	}
 }
 
 export function $(selector) {
@@ -76,3 +120,25 @@ $.create = (tagname, classes = '') => {
 	}
 	return $(el);
 };
+
+
+export function nextSelection(key, {row, col}, MAX_ROW_VALUE) {
+	const MIN_VALUE = 0;
+	const MAX_COL_VALUE = 25;
+	switch (key) {
+	case 'Enter':
+	case 'ArrowDown':
+		row = row + 1 > MAX_ROW_VALUE ? 0 : row + 1
+		break;
+	case 	'Tab':
+		col = col + 1 > MAX_COL_VALUE ? 0 : col + 1
+		break;
+	case 'ArrowUp':
+		row = row - 1 < MIN_VALUE ? MIN_VALUE : row - 1
+		break;
+	}
+
+	return `[data-id="${row}:${col}"]`
+}
+
+
